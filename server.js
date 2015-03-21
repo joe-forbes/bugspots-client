66bc2f32ec4f737c4c513598822f827335899f86
thejsf <joe.forbes@gmail.com>
2015-03-21 00:29:19 -0500

    convert output to a table

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ var Bugspots = require('bugspots');
 var scanner = new Bugspots();
 
 var processResults = function(err, hotspots) {
-    console.log('hi, joe');
+    hotspots.forEach(function(hotspot) {
+      console.log(hotspot.file + '\t' + hotspot.score);
+    })
 };
 
 var options = {
-    repo: '/home/joe/git/github/django', // this is the location of the repo.
-    branch: 'trunk', // this is the branch you want to scan.
-    depth: 100, // not implemented, as it is not implemented in the gem.
-    regex: '.*' // regular expression to use to match commits to use.
+    repo: '/home/joe/git/github/django/django-trunk',
+    regex: /\b(fix(es|ed)?|close(s|d)?)\b/i
 };
 
 scanner.scan(options, processResults);
