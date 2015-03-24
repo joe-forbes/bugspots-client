f5f52e40a6e0184b4e99d0175571866207ce92b1
thejsf <joe.forbes@gmail.com>
2015-03-23 23:11:46 -0500

    added batch size parameter

--- server.js
+++ server.js
@@ -5,15 +5,22 @@ var Bugspots = require('bugspots');
 
 var scanner = new Bugspots();
 
-var processResults = function(err, hotspots) {
-    hotspots.forEach(function(hotspot) {
-      console.log(hotspot.file + '\t' + hotspot.score);
-    })
+var processResults = function (err, hotspots) {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  hotspots.forEach(function (hotspot) {
+    console.log(hotspot.file  + '\t' + hotspot.fixCommits + '\t' + hotspot.firstCommit.toISOString() + '\t' + hotspot.lastCommit.toISOString() + '\t' + hotspot.score);
+  })
 };
 
 var options = {
-    repo: '/home/joe/git/github/django/django-trunk',
-    regex: /\b(fix(es|ed)?|close(s|d)?)\b/i
+  repo: '/home/joe/git/github/django/django-trunk',
+  regex: /\b(fix(es|ed)?|close(s|d)?)\b/i,
+  useRelativeDates: true,
+  depth: 5000,
+  batchSize: 400
 };
 
 scanner.scan(options, processResults);
